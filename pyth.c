090e5f7de922469ef7a4b4c11a8ccf85b3e52b8e
Evan DePosit <edeposit@pdx.edu>
2020-07-05 19:38:23 -0700

    made function for b square

--- pyth.c
+++ pyth.c
@@ -7,7 +7,7 @@ double max_dub(double a, double b);
 double min_dub(double a, double b);
 double distance(double a, double b);
 void find_third_point(double*, double*);
-void b_square(double*, double*, double*, double*);
+void b_square(double*, double*);
 
 
 int main()
@@ -53,10 +53,10 @@ int main()
     G_triangle (triangleXs[0], triangleYs[0],  triangleXs[1], triangleYs[1], triangleXs[2], triangleYs[2]);
 
     //b leg square
-    //b_square(triangleXs, triangleYs,  
-    G_rgb(158/255.0, 204/255.0, 111/255.0); // green
+    b_square(triangleXs, triangleYs);
+    //G_rgb(158/255.0, 204/255.0, 111/255.0); // green
     double bDist = distance(p[1], q[1]);
-    G_fill_rectangle (max_dub(p[0], q[0]), min_dub(p[1], q[1]), distance(p[0], q[0]), distance(p[1], q[1])) ;
+    //G_fill_rectangle (max_dub(p[0], q[0]), min_dub(p[1], q[1]), distance(p[0], q[0]), distance(p[1], q[1])) ;
      
     //a leg square
     G_rgb(148/255.0, 224/255.0, 254/255.0) ; //blue
@@ -75,79 +75,6 @@ int main()
     int numxy = 4 ;
     G_polygon (x,y,numxy) ;
 
-
-  /*
-   double lowleftx, lowlefty, width, height ;
-   double x[10], y[10] ;
-   double numxy ;
-   double a[20], b[20] ;
-   double numab ;
-   // draw a point
-   G_rgb (1.0, 0.0, 0.0) ; // red
-   G_point (200, 580) ; // hard to see
-
-   
-   // draw a line
-   G_rgb (0.0, 1.0, 0.0) ; // green
-   G_line (0,0, swidth-1, sheight-1) ;
-
-   
-   // aligned rectangles
-   G_rgb (0.0, 0.0, 1.0) ; // blue
-   lowleftx = 200 ; lowlefty = 50 ; width = 10 ; height = 30 ;
-   G_rectangle (lowleftx, lowlefty, width, height) ;
-   lowleftx = 250 ; 
-   G_fill_rectangle (lowleftx, lowlefty, width, height) ;
-
-   
-   // triangles
-   G_rgb (1.0, 1.0, 0.0) ; // yellow
-   G_triangle (10, 300,  40,300,  60,250) ;
-   G_fill_triangle (10,100,  40,100,  60,150) ;
-
-   
-   // circles   
-   G_rgb (1.0, 0.5, 0.0) ; // orange
-   G_circle (100, 300, 75) ;
-   G_fill_circle (370, 200, 50) ;
-
-   
-   // polygons
-   G_rgb (0.0, 0.0, 0.0) ; // black
-   x[0] = 100 ;   y[0] = 100 ;
-   x[1] = 100 ;   y[1] = 300 ;
-   x[2] = 300 ;   y[2] = 300 ;
-   x[3] = 300 ;   y[3] = 100 ;
-   x[4] = 200 ;   y[4] = 175 ;
-   numxy = 5 ;
-   G_polygon (x,y,numxy) ;
-
-
-   G_rgb (0.4, 0.2, 0.1) ; // brown
-   a[0] = 300 ;   b[0] = 400 ;
-   a[1] = 350 ;   b[1] = 450 ;
-   a[2] = 275 ;   b[2] = 500 ;
-   a[3] = 125 ;   b[3] = 400 ;
-   numab = 4 ;
-   G_fill_polygon (a,b,numab) ;
-
-
-
-   //===============================================
-   
-   double p[2], q[2] ;
-
-   G_rgb(1,0,0) ;
-   
-   G_wait_click(p) ;
-   G_fill_circle(p[0],p[1],2) ;
-
-   G_wait_click(q) ;
-   G_fill_circle(q[0],q[1],2) ;   
-
-   G_rgb(0,1,0.5) ;
-   G_line(p[0],p[1], q[0],q[1]) ;
-   */
    
    int key ;   
    key =  G_wait_key() ; // pause so user can see results
@@ -192,7 +119,9 @@ void find_third_point(double * xs, double * ys){
     }
 }
 
-void b_square(double* triXs, double* triYs, double* recXs, double* recYs){
-
+void b_square(double* xs, double* ys){
+    G_rgb(158/255.0, 204/255.0, 111/255.0); // green
+    double bDist = distance(ys[1], ys[0]);
+    G_fill_rectangle (max_dub(xs[0], xs[1]), min_dub(ys[0], ys[1]), bDist, bDist);
 }
 
