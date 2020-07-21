1790d5280330e762b2973f96d61652a3a775d7ef
Evan DePosit <edeposit@pdx.edu>
2020-07-21 14:58:04 -0700

    finished midterm

--- stringBuilder.c
+++ stringBuilder.c
@@ -24,77 +24,140 @@ void shift(int start, int length, int shiftSize);
 void rotate_point(double * point, double detlaAngle);
 void next_point(double * lastPoint, double * nextPoint, double startAngle, double deltaAngle, double forwardLen);
 int string_interpreter(double *xs, double *ys, double forwardLen, double startAngle, double deltaAngle);
-void string_drawer(double * xs, double * ys, int n);
+void string_doodler(double * xs, double * ys, int n);
 double max_dub(double a, double b);
 double min_dub(double a, double b);
 double max_dub_array(double * A, int n);
 double min_dub_array(double * A, int n);
+double avg_dubs(double * A, int n);
+void auto_placer(int swidth, int sheight, double startAngle, double deltaAngle, int depth, 
+double * start, double * forwardLen){
+    int n;
+    double xMin;
+    double xMax;
+    double yMin;
+    double yMax;
+    double percentFillX;
+    double percentFillY;
+    double desiredPercentFill = 0.90;
+    double recXs[4];
+    double recYs[4];
+    double recCenterX;
+    double recCenterY;
+    double shiftX;
+    double shiftY;
+
+    printf("swidth: %d\n", swidth);
+    printf("sheight: %d\n", sheight);
+    printf("startAngle: %lf\n", startAngle*toDegrees);
+    printf("deltaAngle: %lf\n", deltaAngle*toDegrees);
+    printf("depth: %d\n", depth);
+    printf("start: %lf, %lf\n", start[0], start[1]);
+    printf("forwardLen: %lf\n", *forwardLen);
+
+    //string Doodler
+    double xs[10000];
+    double ys[10000];
+    xs[0] = start[0];
+    ys[0] = start[1];
+    n = string_interpreter(xs, ys, *forwardLen, startAngle, deltaAngle);
+    //string_doodler(xs, ys, n);
+
+    //scale
+    xMin = min_dub_array(xs, n);
+    xMax = max_dub_array(xs, n);
+    yMin = min_dub_array(ys, n);
+    yMax = max_dub_array(ys, n);
+    
+    printf("xMin: %lf\n", xMin);
+    printf("xMax: %lf\n", xMax);
+    printf("yMin: %lf\n", yMin);
+    printf("yMax: %lf\n", yMax);
+
+    percentFillX =  (xMax - xMin) / swidth;
+    percentFillY = (yMax - yMin) / sheight;
+    //find limiting length
+    if( percentFillX > percentFillY){
+        printf("width is limiting\n");
+        *forwardLen = *forwardLen * desiredPercentFill / percentFillX;
+    }
+    else{
+        printf("height is limiting\n");
+        *forwardLen = *forwardLen * desiredPercentFill / percentFillY;
+    }
+    //translate
+    string_interpreter(xs, ys, *forwardLen, startAngle, deltaAngle);
+    xMin = min_dub_array(xs, n);
+    xMax = max_dub_array(xs, n);
+    yMin = min_dub_array(ys, n);
+    yMax = max_dub_array(ys, n);
+
+    recXs[0] = xMin;
+    recXs[1] = xMax;
+    recXs[2] = xMax;
+    recXs[3] = xMin;
+
+    recYs[0] = yMin;
+    recYs[1] = yMin;
+    recYs[2] = yMax;
+    recYs[3] = yMax;
+    
+    //G_rgb (1.0, 0.5, 0.0) ; // orange
+    //G_polygon(recXs, recYs, 4);
+
+    recCenterX = avg_dubs(recXs, 4);
+    recCenterY = avg_dubs(recYs, 4);
+    
+    //draw circ at center of rec
+    //G_rgb (1.0, 0.5, 0.0) ; // orange
+    //G_fill_circle (recCenterX, recCenterY, 3) ;
+
+    shiftX = swidth/2.0 - recCenterX;
+    shiftY = sheight/2.0 - recCenterY;
+
+    //run string intepreter again with new start
+    start[0] = start[0] + shiftX;
+    start[1] = start[1] + shiftY;
+    xs[0] = start[0];
+    ys[0] = start[1];
+    string_interpreter(xs, ys, *forwardLen, startAngle, deltaAngle);
+    //or just write new function to shift xs and ys
+
+    //string_doodler(xs, ys, n);
+
+    //test modfitying by reference
+    //start[0] = 0.0;
+    //start[1] = 1.0;
+    //*forwardLen = 3;
+}
 
 int main()
 {
-    //seg faults at depth 12
-    int depth = 3;
-    double startAngle = 0;
-    double deltaAngle = 60 * toRadians;
-    double forwardLen=8;
-    double startX = 200;
-    double startY = 200;
+    //needed by auto placer
     int swidth = 600;
     int sheight = 600 ;
+    double startAngle = 0;
+    double deltaAngle = 60 * toRadians;
+    int depth = 3;
+    struct Node* rules;
+
+    //modifed by auto placer
+    double start[2];
+    start[0] = 200; 
+    start[1] = 200;
+    double forwardLen=1;
+
+    // not needed by auto placer
     int key ;   
     struct Node* node;
-    struct Node* rules;
     char axiom[10] = "F";
-    double xs[10000];
-    double ys[10000];
     int pointsLen;
 
-    
     //make list of rules 
     node = new_rule('F', "F+F--F+F");
     rules = node;
 
-
-    // ---- String builder ----
-
-
-//   // old rule strinb builder test test 
-//     struct Node* nodeTest = new_rule('F', "-F+F-");
-//     struct Node* rulesTest = nodeTest;
-//     printf("old test\n");
-//     printf("rule: %s\n", rulesTest->rule);
-//     //test 1
-//     u[0]='\0';
-//     strcpy(u, "++F");
-//     printf("axiom: %s\n", u);
-//     string_builder(rulesTest, 1);
-//     printf("\nresult 1: %s\n", u);
-//
-//     //test 2
-//     u[0]='\0';
-//     strcpy(u, "++F");
-//     printf("axiom: %s\n", u);
-//     string_builder(rulesTest, 2);
-//     printf("\nresult 2:%s\n", u);
-
-///    //new rule string builder tests
-///    printf("\n\n new rule test\n");
-///    //stringBuilder test 1
-//       u[0] = '\0';
-//       strcpy(u, "F");
-///    printf("test 1\n");
-///    printf("axiom: %s\n", u);
-///    printf("rule: %s\n", rules->rule);
-///    string_builder(rules, 1);
-///    char str1[10] = "F+F--F+F";
-///    printf("result:  %s\n", u);
-///    printf("expected:%s\n", str1);
-    //stringBuilder test 1
-    //char str2[50] = "F+F--F+F+F+F--F+F--F+F--F+F+F+F--F+F";
-    //stringBuilder test 2
-    //char str3[200] = "F+F--F+F+F+F--F+F--F+F--F+F+F+F--F+F+F+F--F+F+F+F--F+F--F+F--F+F+F+F--F+F--F+F--F+F+F+F--F+F--F+F--F+F+F+F--F+F+F+F--F+F+F+F--F+F--F+F--F+F+F+F--F+F";
-   
-    //draw snow flake
+    //string builder
     u[0] ='\0';
     strcpy(u, "F");
     string_builder(rules, depth);
@@ -102,25 +165,26 @@ int main()
     //deltaAngle = 45 * toRadians;
     //strcpy(u, "++F++F++F++F");
 
-    //draw something from string
-    //string_interpreter(*xs, *ys, *start, forwardLen, startAngle, deltaAngle);
-    xs[0] = startX;
-    ys[0] = startY;
-    pointsLen = string_interpreter(xs, ys, forwardLen, startAngle, deltaAngle);
 
-    //test print all points
-    printf("\n\npoints print out\n");
-    for(int i=0; i<pointsLen; i++){
-        printf("%lf %lf\n", xs[i], ys[i]);
-    }
-
-    //graphics 
+    //init graphics 
     G_init_graphics (swidth,sheight) ;  // interactive graphics
     G_rgb (0.3, 0.3, 0.3) ; // dark gray
     G_clear () ;
     
-
-    string_drawer(xs, ys, pointsLen);
+    //auto placer
+    //void auto_placer(int swidth, int sheight, double startAngle, double detlaAngle, 
+    //int depth, double * start, double * forwardLen){
+    auto_placer(swidth, sheight, startAngle, deltaAngle, depth, start, &forwardLen); 
+    printf("start: %lf, %lf\n", start[0], start[1]);
+    printf("forwardLen: %lf\n", forwardLen);
+    
+    //string Doodler
+    double xs[10000];
+    double ys[10000];
+    xs[0] = start[0];
+    ys[0] = start[1];
+    pointsLen = string_interpreter(xs, ys, forwardLen, startAngle, deltaAngle);
+    string_doodler(xs, ys, pointsLen);
 
     key ;   
     key =  G_wait_key() ; // pause so user can see results
@@ -168,7 +232,6 @@ void string_builder(struct Node* rules, int maxDepth){
     int i;
     int j;
     
-
     //one pass
     for(int depth=0; depth< maxDepth; depth++){
         i = 0;
@@ -295,8 +358,8 @@ int string_interpreter(double *xs, double *ys, double forwardLen, double startAn
         lastPoint[0] = xs[0];
         lastPoint[1] = ys[0];
         next_point(lastPoint, nextPoint, startAngle, 0, f*forwardLen);
-        printf("next_Point(lastPoint=(%lf, %lf), nextPoint=(%lf, %lf), startAngle=%lf, turn=%lf, f*len=%d*%lf\n", 
-                lastPoint[0], lastPoint[1], nextPoint[0], nextPoint[1], startAngle, turn, f, forwardLen);
+        //printf("next_Point(lastPoint=(%lf, %lf), nextPoint=(%lf, %lf), startAngle=%lf, turn=%lf, f*len=%d*%lf\n", 
+        //        lastPoint[0], lastPoint[1], nextPoint[0], nextPoint[1], startAngle, turn, f, forwardLen);
         xs[j] = nextPoint[0];
         ys[j] = nextPoint[1];
         lastPoint[0] = nextPoint[0];
@@ -306,7 +369,6 @@ int string_interpreter(double *xs, double *ys, double forwardLen, double startAn
         turn = 0;
 
     }
-    printf("\n\n");
     
     while(c != '\0'){
         // turn
@@ -327,8 +389,8 @@ int string_interpreter(double *xs, double *ys, double forwardLen, double startAn
         }
         //move
         next_point(lastPoint, nextPoint, startAngle, turn, f*forwardLen);
-        printf("next_Point(lastPoint=(%lf, %lf), nextPoint=(%lf, %lf), startAngle=%lf, turn=%lf, f*len=%d*%lf\n", 
-                lastPoint[0], lastPoint[1], nextPoint[0], nextPoint[1], startAngle*toDegrees, turn*toDegrees, f, forwardLen);
+        //printf("next_Point(lastPoint=(%lf, %lf), nextPoint=(%lf, %lf), startAngle=%lf, turn=%lf, f*len=%d*%lf\n", 
+        //        lastPoint[0], lastPoint[1], nextPoint[0], nextPoint[1], startAngle*toDegrees, turn*toDegrees, f, forwardLen);
 
         xs[j] = nextPoint[0];
         ys[j] = nextPoint[1];
@@ -341,7 +403,7 @@ int string_interpreter(double *xs, double *ys, double forwardLen, double startAn
     return j;
 }
 
-void string_drawer(double * xs, double * ys, int n){
+void string_doodler(double * xs, double * ys, int n){
     G_rgb (0.0, 1.0, 0.0) ; // green
     for(int i = 0; i < n-1; i++){
        G_line (xs[i],ys[i], xs[i+1], ys[i+1]);
@@ -380,3 +442,10 @@ double min_dub_array(double * A, int n){
     return min;
 }
 
+double avg_dubs(double * A, int n){
+    double accum = 0.0;
+    for(int i=0; i<n; i++){
+        accum = accum + A[i];
+    }
+    return accum / n;
+}
